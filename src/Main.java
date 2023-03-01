e72b1dbacc8e196e6cbfdeb2cfa8c016b2e5f0e8
edelwiw <edelwiw@gmail.com>
2023-03-01 22:22:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// times crying while writing this code = 0
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This will become a great collection manager... In future");
+    }
+}
